6512c61a4a070c6e6116a3ce08bf518980bc6821
Izzar Suly Nashrudin <152323638+avezoor@users.noreply.github.com>
2025-07-17 06:40:20 +0700

    meme up

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://your-domain.com",
+      url: "https://meme.avezoor.my.id",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
